68f4aea73917eca430b85c3f9df4062999301a90
H M Kashif Ali <hmkashifali@Kashifs-MacBook-Pro.local>
2019-07-12 22:45:12 +0500

    Compact method is used instead of reject method with a block

--- 7/remove_nil.rb
+++ 7/remove_nil.rb
@@ -4,8 +4,6 @@ arr = ['one', 'two', '', nil ,'four', '', 'six', 'seven', 'eight', '9', '10']
 
 print "Before: #{arr}\n"
 
-arr.reject! do |item|
-  item.nil? || item.empty?
-end
+arr.compact!
 
 print "After: #{arr}\n"
