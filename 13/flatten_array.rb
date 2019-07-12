18cde83e4e3a574de56785e07535f8f11cd36e02
H M Kashif Ali <hmkashifali@Kashifs-MacBook-Pro.local>
2019-07-12 21:24:26 +0500

    flattens a multilevel array to less multilevel array

---
+++ 13/flatten_array.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+arr = [1, [2, [9, [6, [7, 3, 4, 8, 5, 0]]]]]
+
+print "Original array: #{arr}\n"
+
+print "Result : #{arr.flatten!(1)}\n"
+print "Result : #{arr.flatten!(1)}\n"
+print "Result : #{arr.flatten!(1)}\n"
+print "Result : #{arr.flatten!(1)}\n"
