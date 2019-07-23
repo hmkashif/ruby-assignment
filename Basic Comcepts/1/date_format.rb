f51a9993789974354c60580fffc9c27d85c96b89
H M Kashif Ali <hmkashifali@Kashifs-MacBook-Pro.local>
2019-07-23 16:34:48 +0500

    Date and time has been printed in the required formats. Extra formats have been removed

--- Basic Comcepts/1/date_format.rb
+++ Basic Comcepts/1/date_format.rb
@@ -3,20 +3,6 @@
 date = Time.new
 values = date.to_a
 
-puts 'Different formats are: \n'
-puts date.strftime('%a %b %d, %Y %I:%M:%S %p')
-puts date.strftime('%A %B %d, %Y %H:%M:%S')
-puts date.strftime('%d %m %Y %I:%M:%S %p')
-puts date.strftime('%d/%m/%y %H:%M:%S')
-puts date.strftime('%d-%m-%Y %I %M %S %p')
-puts date.strftime('%x')
-puts date.strftime('%X')
-
-puts '------------'
-
-puts date.to_s
-puts date.localtime
-puts date.ctime
-puts Time.local(*values)
-puts Time.utc(*values)
-puts Time.gm(*values)
+puts 'Current Date and Time: ' + date.strftime('%-d-%m-%Y %l:%M %p')
+puts 'Current Date and Time: ' + date.strftime('%-d-%m-%Y %H:%M')
+puts 'Current Date and Time: ' + date.strftime('%A %-d %B %Y %l:%M %p')
